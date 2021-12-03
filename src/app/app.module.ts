14fe111a8e37cd2eca850da12d5c4bf9c2298455
AkhtarGul <akhtar0316169@gmail.com>
2021-12-04 00:20:13 +0500

    2 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PropertybindingComponent } from './propertybinding/propertybinding.component';
+import { ClassStyleComponent } from './class-style/class-style.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,PropertybindingComponent,ClassStyleComponent,EventBindingComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
