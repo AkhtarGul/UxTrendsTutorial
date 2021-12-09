5569bac51bf4c5f8a3c48a8ff5de7e632107c928
AkhtarGul <akhtar0316169@gmail.com>
2021-12-10 01:37:49 +0500

    3 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,33 @@ import { ClassStyleComponent } from './class-style/class-style.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
 import { TwoWayBindngComponent } from './two-way-bindng/two-way-bindng.component';
 import { NgswtichComponent } from './ngswtich/ngswtich.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'contact', component: ContactComponent },
+];
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,PropertybindingComponent,ClassStyleComponent,EventBindingComponent,TwoWayBindngComponent,NgswtichComponent],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    PropertybindingComponent,
+    ClassStyleComponent,
+    EventBindingComponent,
+    TwoWayBindngComponent,
+    NgswtichComponent,
+    ProductComponent,
+    ContactComponent,
+    AboutComponent,
+    HomeComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
